2f2406c2789d04846483758b898b5966f6124b61
Christopher Fryman <farfromguam@gmail.com>
2013-07-01 16:30:58 -0500

    Devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ChatMittens::Application.routes.draw do
+
+  devise_for :users
+
+  root :to => "home#index"
+
+  resources :home
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
