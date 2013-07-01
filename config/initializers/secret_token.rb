07302c9530465e4f42b46a1d71495e8899ee7b14
Christopher Fryman <farfromguam@gmail.com>
2013-07-01 16:01:41 -0500

    rails new . --database=postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatMittens::Application.config.secret_token = 'f8a920b294c456843f5b96b3cf07d954f31567164282c6ea224ddf10a079dbee9648ee20895895ac3613b98a9e5024196ce815fd41a98be9e65d767fa61c41ae'
